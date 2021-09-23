1513f86ce4cc402172cbf7c15d8a4398c372a05c
韩笑 <xiao.han02@mihoyo.com>
2021-09-23 19:23:54 +0800

    fix-1

---
+++ Sources/MyLibrary/Animal.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by 韩笑 on 2021/9/23.
+//
+
+import Foundation
+
+public struct Animal {
+    public var name = "Dog"
+}
+
+public func animalLog() {
+    print("animal.name")
+}
