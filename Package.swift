d229cf308ecba187cd9f000024bcd416b65cd26f
韩笑 <xiao.han02@mihoyo.com>
2021-09-23 19:26:18 +0800

    fix-2

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyLibrary",
-            targets: ["MyLibrary", "Module"])
+            targets: ["MyLibrary"]),
+        .library(
+            name: "Module",
+            targets: ["Module"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
